d06f797075ab7376264996f6a68d9a094b542917
RasmusHAMK <142879532+RasmusLaitilaHAMK@users.noreply.github.com>
2025-09-11 14:55:49 +0300

    Update IiroKäki_Heurestics.py

--- 2048/2048-python/IiroKäki_Heurestics.py
+++ 2048/2048-python/IiroKäki_Heurestics.py
@@ -4,3 +4,13 @@ import constants as c
 import logic
 import random
 from multiprocessing.pool import ThreadPool
+import AI_heuristics as AIH
+
+pool = ThreadPool(4)
+transposition_table = {}
+
+commands = {c.KEY_UP: logic.up,
+            c.KEY_DOWN: logic.down,
+            c.KEY_LEFT: logic.left,
+            c.KEY_RIGHT: logic.right}
+
