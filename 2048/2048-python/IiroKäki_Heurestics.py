ba9761b2fa74c7f4b37cca86cbc5076b78ebef69
RasmusHAMK <142879532+RasmusLaitilaHAMK@users.noreply.github.com>
2025-09-11 14:55:12 +0300

    Defaults added to heurestic files

---
+++ 2048/2048-python/IiroKäki_Heurestics.py
@@ -0,0 +1,6 @@
+import numpy as np
+import copy
+import constants as c
+import logic
+import random
+from multiprocessing.pool import ThreadPool
---
+++ 2048/2048-python/RasmusLaitila_Heurestics.py
@@ -0,0 +1,17 @@
+import numpy as np
+import copy
+import constants as c
+import logic
+import random
+from multiprocessing.pool import ThreadPool
+import AI_heuristics as AIH
+
+
+pool = ThreadPool(4)
+transposition_table = {}
+
+commands = {c.KEY_UP: logic.up,
+            c.KEY_DOWN: logic.down,
+            c.KEY_LEFT: logic.left,
+            c.KEY_RIGHT: logic.right}
+
